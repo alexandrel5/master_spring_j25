b2a888173575cc1fadd8dc0645972ba1f27620cf
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-26 09:57:22 -0300

    @RequestMapping

--- section3/backend/src/main/java/com/eazybytes/backend/controller/UserController.java
+++ section3/backend/src/main/java/com/eazybytes/backend/controller/UserController.java
@@ -2,13 +2,18 @@ package com.eazybytes.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api/dummy/users")
 public class UserController {
 
-    @GetMapping({"/api/dummy/users/{userId}/posts/{postId}", "/api/dummy/users/{userId}"})
-    public String searchUserPostWithMultiPathVariables(@PathVariable Long userId, @PathVariable(required = false) Long postId) {
+    @GetMapping({"/{userId}/posts/{postId}", "/api/dummy/users/{userId}"})
+    public String searchUserPostWithMultiPathVariables(@PathVariable Long userId,
+                                                       @PathVariable(required = false) Long postId) {
         String response;
         if(postId == null) {
             response = "Fetched user with id "+userId;
@@ -16,5 +21,17 @@ public class UserController {
             response = "Fetched user with id " + userId + " and post id " + postId;
         }
         return response;
+    }
+
+    @GetMapping({"/{userId}/orders/{orderId}"})
+    public String searchUserOrderWithMultiPathVariables(@PathVariable(name = "userId") Long customerId,
+                                                       @PathVariable Long orderId) {
+        return "Fetched user with id " + customerId + " and order id " + orderId;
+    }
+
+    @GetMapping({"/{userId}/address/{addressId}"})
+    public String searchUserAddressWithMultiPathVariables(@PathVariable Map<String, String> pathVariablesMap) {
+        return "Fetched user with id " +
+                pathVariablesMap.get("userId") + " and address id " + pathVariablesMap.get("addressId");
     }
 }
