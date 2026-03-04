adf7088aaa31884e9935a7b0c7c36e1564e5e60d
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-04 17:48:25 -0300

    Custom Security configuration

--- section10/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
+++ section10/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,10 +17,12 @@ public class JobPortalSecurityConfig {
 
     @Bean
     @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+    SecurityFilterChain customSecurityFilterChain(HttpSecurity http) {
         return http.csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((requests) -> requests.anyRequest()
-                        .permitAll())
+                .authorizeHttpRequests((requests) ->
+//                        requests.requestMatchers("/api/companies/public").permitAll()
+//                            .requestMatchers("/api/contacts/public").permitAll())
+                        requests.requestMatchers(RegexRequestMatcher.regexMatcher(".*public$")).permitAll())
                         .formLogin(flc -> flc.disable())//to disable form login use flc -> flc.disable()
                         .httpBasic(withDefaults())//To disable httpBasic use hbc -> hbc.disable()
                         .build();
