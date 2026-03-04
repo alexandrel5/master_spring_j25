2bf010bb810b1b08e939fb2d70e6da366df38bb5
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-04 18:04:40 -0300

    Custom Security configuration to external file

--- section10/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
+++ section10/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
@@ -1,28 +1,46 @@
 package com.eazybites.jobportal.security;
 
-import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
+import java.util.List;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class JobPortalSecurityConfig {
 
+    @Qualifier("publicPaths")
+    private final List<String> publicPaths;
+
+    @Qualifier("securedPaths")
+    private final List<String> securedPaths;
+
     @Bean
-    @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain customSecurityFilterChain(HttpSecurity http) {
         return http.csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((requests) ->
-//                        requests.requestMatchers("/api/companies/public").permitAll()
-//                            .requestMatchers("/api/contacts/public").permitAll())
-                        requests.requestMatchers(RegexRequestMatcher.regexMatcher(".*public$")).permitAll())
+                .authorizeHttpRequests((requests) ->{
+                    publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+                    securedPaths.forEach(path -> requests.requestMatchers(path).authenticated());
+                    requests.anyRequest().denyAll();
+                })
+////                        requests.requestMatchers("/api/companies/public").permitAll()
+////                            .requestMatchers("/api/contacts/public").permitAll())
+//                        requests.requestMatchers(RegexRequestMatcher.regexMatcher(".*public$")).permitAll()
+//                                .requestMatchers("/api/swagger-ui.html",
+//                                            "/swagger-ui/**",
+//                                            "/api/v3/api-docs/**",
+//                                            "/swagger-resources/**",
+//                                            "/swagger-ui.html",
+//                                            "/webjars/**").permitAll())
                         .formLogin(flc -> flc.disable())//to disable form login use flc -> flc.disable()
                         .httpBasic(withDefaults())//To disable httpBasic use hbc -> hbc.disable()
                         .build();
