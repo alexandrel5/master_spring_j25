470487c96a30788cc275d2a0e50f5e008e7c0ea0
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-06 10:27:03 -0300

    section12

---
+++ section12/jobportal/src/main/java/com/eazybites/jobportal/company/controller/CompanyController.java
@@ -0,0 +1,31 @@
+package com.eazybites.jobportal.company.controller;
+
+import com.eazybites.jobportal.dto.CompanyDto;
+import com.eazybites.jobportal.company.service.ICompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final ICompanyService companyService;
+
+//    @Autowired// Optional
+//    private CompanyController(ICompanyService companyService) {
+//        this.companyService = companyService;
+//    }
+
+    @GetMapping(path = "/public", version = "1.0")
+    public ResponseEntity<List<CompanyDto>> getAllCompanies(){
+
+        List<CompanyDto> companiesList = companyService.getALlCompany();
+        return ResponseEntity.ok().body(companiesList);
+    }
+}
