316d7d0eeac3b1bcf363eb7c401eabd90dc515b6
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-17 16:43:16 -0300

    Building User Profile Job action with spring Data JPA

--- section19/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
+++ section19/jobportal/src/main/java/com/eazybites/jobportal/security/JobPortalSecurityConfig.java
@@ -50,6 +50,9 @@ public class JobPortalSecurityConfig {
     @Qualifier("employerPaths")
     private final List<String> employerPaths;
 
+    @Qualifier("jobseekerPaths")
+    private final List<String> jobseekerPaths;
+
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity http) {
         return http.
@@ -61,6 +64,7 @@ public class JobPortalSecurityConfig {
                     publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
                     adminPaths.forEach(path -> requests.requestMatchers(path).hasRole("ADMIN"));
                     employerPaths.forEach(path -> requests.requestMatchers(path).hasRole("EMPLOYER"));
+                    jobseekerPaths.forEach(path -> requests.requestMatchers(path).hasRole("JOB_SEEKER"));
                     securedPaths.forEach(path -> requests.requestMatchers(path).authenticated());
                     requests.anyRequest().denyAll();
                 })
--- section19/jobportal/src/main/java/com/eazybites/jobportal/user/service/IUserService.java
+++ section19/jobportal/src/main/java/com/eazybites/jobportal/user/service/IUserService.java
@@ -1,7 +1,10 @@
 package com.eazybites.jobportal.user.service;
 
-import com.eazybites.jobportal.dto.UserDto;
+import com.eazybites.jobportal.dto.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IUserService {
@@ -27,4 +30,24 @@ public interface IUserService {
      * @return updated UserDto
      */
     UserDto assignCompanyToEmployer(Long userId, Long companyId);
+
+    ProfileDto createOrUpdateProfile(String userEmail, String profileJson, MultipartFile profilePicture, MultipartFile resume) throws JsonProcessingException;
+
+    ProfileDto getProfile(String userEmail);
+
+    ProfileDto getProfilePicture(String userEmail);
+
+    ProfileDto getResume(String userEmail);
+
+    JobDto saveJob(String userEmail, Long jobId);
+
+    void unsaveJob(String userEmail, Long jobId);
+
+    List<JobDto> getSavedJobs(String userEmail);
+
+    JobApplicationDto applyForJob(String userEmail, ApplyJobRequestDto request);
+
+    void withdrawApplication(String userEmail, Long jobId);
+
+    List<JobApplicationDto> getJobSeekerApplications(String userEmail);
 }
