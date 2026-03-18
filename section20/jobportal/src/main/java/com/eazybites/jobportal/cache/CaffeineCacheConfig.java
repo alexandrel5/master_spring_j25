6c50f89a77c4dbafddecad4f79706da366ef05f4
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-18 15:37:24 -0300

    Reading Config Properties the Classic Way

--- section20/jobportal/src/main/java/com/eazybites/jobportal/cache/CaffeineCacheConfig.java
+++ section20/jobportal/src/main/java/com/eazybites/jobportal/cache/CaffeineCacheConfig.java
@@ -1,33 +1,54 @@
 package com.eazybites.jobportal.cache;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+@Configuration
 public class CaffeineCacheConfig {
+
+    @Value("${cache.jobs.ttl-minutes:5}")
+    private int JobsCacheTtlMinutes;
+
+    @Value("${cache.jobs.max-size:2000}")
+    private int jobsCacheMaxSize;
+
+    @Value("${cache.companies.ttl-minutes:5}")
+    private int companiesCacheTtlMinutes;
+
+    @Value("${cache.compnaies.max-size:100}")
+    private int companiesCacheMaxSize;
+
+    @Value("${cache.roles.ttl-days:2}")
+    private int rolesCacheTtlDays;
+
+    @Value("${cache.roles.max-size:50}")
+    private int rolesCacheMaxSize;
 
     @Bean
     public CacheManager caffeineCacheManager() {
 
         CaffeineCache jobsCache = new CaffeineCache("jobs",
                 Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .maximumSize(5000)
+                        .expireAfterWrite(JobsCacheTtlMinutes, TimeUnit.MINUTES)
+                        .maximumSize(jobsCacheMaxSize)
                         .build());
         CaffeineCache companiesCache = new CaffeineCache("companies",
                 Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .maximumSize(500)
+                        .expireAfterWrite(companiesCacheTtlMinutes, TimeUnit.MINUTES)
+                        .maximumSize(companiesCacheMaxSize)
                         .build());
         CaffeineCache rolesCache = new CaffeineCache("role",
                 Caffeine.newBuilder()
-                        .expireAfterWrite(1, TimeUnit.DAYS)
-                        .maximumSize(100)
+                        .expireAfterWrite(rolesCacheTtlDays, TimeUnit.DAYS)
+                        .maximumSize(rolesCacheMaxSize)
                         .build());
 
         SimpleCacheManager manager = new SimpleCacheManager();
