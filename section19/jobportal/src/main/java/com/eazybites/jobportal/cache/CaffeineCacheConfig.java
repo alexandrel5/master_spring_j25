e984eaaa6172bff111173c7b098c8d9f4de4b079
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-03-18 15:28:41 -0300

    Annotation inside CaffeineCacheConfig.java

--- section19/jobportal/src/main/java/com/eazybites/jobportal/cache/CaffeineCacheConfig.java
+++ section19/jobportal/src/main/java/com/eazybites/jobportal/cache/CaffeineCacheConfig.java
@@ -5,10 +5,12 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+@Configuration
 public class CaffeineCacheConfig {
 
     @Bean
